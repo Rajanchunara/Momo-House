f287c3a308ea3cc67df3c31653edb8b1a81af8cd
rajan <rajan0762115@gmail.com>
2025-02-17 11:07:26 +0545

    new file

--- src/Pages/OurMenu.jsx
+++ src/Pages/OurMenu.jsx
@@ -1,5 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom';
+import menu from '../assets/Menu/menu.png'
+import momo from '../assets/Image/momo.png'
+import { ImMobile } from "react-icons/im";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { AiFillTikTok } from "react-icons/ai";
+import { FaLinkedin } from "react-icons/fa";
+import { FaSquareTwitter } from "react-icons/fa6";
+import { IoLogoYoutube } from "react-icons/io";
+
 
 function OurMenu() {
 
@@ -17,24 +27,87 @@ function OurMenu() {
 
   return (
     <div>
-      {product.length > 0 ? (
-        <div className='flex p-[30px] flex-wrap gap-8 justify-center'>
-          {product.map((item) => {
-            return (
-              <NavLink
-                key={item.name}
-                to={`/ProductDescription/${item.id}`}>
-                <div key={item.name} className='h-48 w-52 flex flex-col justify-center items-center shadow-2xl shadow-gray-700 rounded-[20px] gap-2 hover:bg-gray-100' >
-                  <img src={item.image} alt="" className='h-[100px] rounded-2xl ' />
-                  <h1>{item.name}</h1>
-                  <h1>Rs: {item.caloriesPerServing}</h1>
+
+      <div className='mt-[30px]'>
+        <h1 className='text-[56px] font-parisienne text-teal-700 flex justify-center'>Our Menu</h1>
+        <p className='text-[31px] text-orange-600 flex flex-col items-center font-bold'>Our menu is more than just momos,
+          <span className='text-black'> with a variety of dishes to cater to all tastes and preferences.</span></p>
+
+        <div className='flex justify-center'>
+          <div className='h-[442px] w-[1100px] bg-gray-100 mt-15 rounded-[16px] flex flex-col justify-center items-center'>
+            <h1 className='text-[31px] text-teal-700'>Scan the QR code</h1>
+            <p className='text-gray-500'>You can also check the allergy advices using your phone as well</p>
+            <div className='mt-[40px]'>
+              <img src={menu} alt="" className='h-[168px] w-[168px] border-4 rounded-[12px]' />
+              <h1 className='flex text-orange-600 text-[30px] font-bold'>SCAN <span className='text-black'>ME!</span><ImMobile className='text-black mt-[10px]' /> </h1>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div className='mt-[40px]'>
+        <h1 className='text-[50px] font-bold text-orange-500 flex justify-center gap-4 underline'>Our <span className='text-black'>Menu</span></h1>
+        {product.length > 0 ? (
+          <div className='flex p-[30px] flex-wrap gap-8 justify-center'>
+            {product.map((item) => {
+              return (
+                <div key={item.name} className='border border-gray-400 h-[400px] w-[320px] flex flex-col justify-center items-center shadow-2xl shadow-gray-700 rounded-[20px]' >
+                  <img src={item.image} alt="" className='h-[240px] rounded-2xl ' />
+                  <h1 className='mt-[20px] font-bold'>{item.name}</h1>
+                  <h1 className='text-red-600'>Rs: {item.caloriesPerServing}</h1>
+                  <NavLink
+                    key={item.name}
+                    to={`/ProductDescription/${item.id}`}>
+                    <button className='mt-[20px] h-[50px] w-[150px] rounded-[50px] bg-orange-500 text-white hover:bg-amber-700'>View More</button>
+                  </NavLink>
                 </div>
-              </NavLink>
-            )
+              )
+            })}
+          </div>) : (<div>Loading...</div>)}
+      </div>
 
-          })}
 
-        </div>) : (<div>Loading...</div>)}
+      <div className='mt-[40px] h-[400px] bg-gray-100 p-[50px]'>
+        <div className=' flex justify-between items-center w-[1128px] h-[200px]'>
+          <div className='w-[408px] h-[192px]'>
+            <img src={momo} alt="" className='w-[152px] h-[48px]' />
+            <p className='text-gray-500'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Cupiditate nisi iusto unde quis quibusdam impedit ex facilis doloremque quaerat laudantium.</p>
+          </div>
+          <div className='w-[168px] h-[158px] flex flex-col justify-center'>
+            <h1 className='text-[25px] text-teal-700'>Momos</h1>
+            <h3>About Us</h3>
+            <h3>Our Menu</h3>
+            <h3>Our Services</h3>
+            <h3>Contact Us</h3>
+          </div>
+          <div className='w-[168px] h-[158px] flex flex-col items-start'>
+            <h1 className='text-[25px] text-teal-700'>Legals</h1>
+            <h3>Terms & Conditions</h3>
+            <h3>Privacy Policy</h3>
+            <h3>Supports</h3>
+          </div>
+          <div className='w-[168px] h-[158px] space-y-2' >
+            <h1 className='text-[25px] text-teal-700'>Follow Us</h1>
+            <div className='flex gap-3'>
+              <FaFacebook className='h-[35px] w-[35px] text-gray-500' />
+              <FaLinkedin className='h-[35px] w-[35px] text-gray-500' />
+              <FaSquareTwitter className='h-[35px] w-[35px] text-gray-500' />
+            </div>
+            <div className='flex gap-3'>
+              <IoLogoYoutube className='h-[35px] w-[35px] text-gray-500' />
+              <FaInstagram className='h-[35px] w-[35px] text-gray-500' />
+              <AiFillTikTok className='h-[35px] w-[35px] text-gray-500' />
+            </div>
+          </div>
+        </div>
+        <div className='border-b-1 border-gray-500 mt-[50px]'></div>
+        <div>
+          <h1 className='text-gray-500 flex justify-center mt-[30px]'>Copyright ©2025 Everest Momo Pvt Ltd. All Rights Reserved.</h1>
+        </div>
+      </div>
+
     </div>
   )
 }
