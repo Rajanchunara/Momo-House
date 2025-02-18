6e880fb1f31486842dfcc57977a8d68393b6d059
rajan <rajan0762115@gmail.com>
2025-02-18 12:26:21 +0545

    added

--- src/Pages/CartPage.jsx
+++ src/Pages/CartPage.jsx
@@ -2,10 +2,29 @@ import React, { useContext } from 'react'
 import { CartContext } from '../Context/CartContext/CartProvider'
 
 function CartPage() {
-  const {state} = useContext(CartContext)
+  const { state } = useContext(CartContext)
   console.log(state);
   return (
-    <div>CartPage</div>
+    <div>
+      <h1>Cart page</h1>
+      <div>
+        {state.cartItem ? (
+          <div>
+            {state.cartItem.map((item) => {
+              return (
+                <div key={item.id}>
+                  <h1>{item.name}</h1>
+                  <img className="h-40" src={item.image} alt="" />
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div>Item Not Found On Cart</div>
+        )}
+      </div>
+    </div>
+
   )
 }
 
