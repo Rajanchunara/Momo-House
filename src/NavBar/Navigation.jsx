f18ee9895a6156c7e8a6911c1046c6acdfe8fc64
rajan <rajan0762115@gmail.com>
2025-02-19 12:36:09 +0545

    added new section

--- src/NavBar/Navigation.jsx
+++ src/NavBar/Navigation.jsx
@@ -1,25 +1,41 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
 import momo from '../assets/Image/momo.png'
 import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { AiFillTikTok } from "react-icons/ai";
 import { BsCart4 } from "react-icons/bs";
+import { CartContext } from '../Context/CartContext/CartProvider';
 
 function Navigation() {
+
+
+  const { state } = useContext(CartContext)
+  console.log(state.cartItem);
+  const totalCartItem = state.cartItem.reduce((acc, item) => {
+    return acc + item.qty;
+  }, 0)
+  console.log(totalCartItem);
+
+
   return (
     <div>
       <div className='h-[90px] shadow-2xs shadow-gray-300  pl-[110px] flex items-center justify-center bg-white'>
-      <div>
-            <img src={momo} alt="" className='h-[45px]' />
-          </div>
+        <div>
+          <img src={momo} alt="" className='h-[45px]' />
+        </div>
         <div className='pl-[20px] space-x-[30px] flex'>
-        <NavLink to='/' className='hover:text-orange-500 text-gray-500'>Home</NavLink>
+          <NavLink to='/' className='hover:text-orange-500 text-gray-500'>Home</NavLink>
           <NavLink to='/aboutus' className='hover:text-orange-500 text-gray-500'>About Us</NavLink>
           <NavLink to='/ourmenu' className='hover:text-orange-500 text-gray-500' >Our Menu</NavLink>
           <NavLink to='/ourservices' className='hover:text-orange-500 text-gray-500'>Our Services</NavLink>
           <NavLink to='/allergyadvice' className='hover:text-orange-500 text-gray-500'>Allergy Advice</NavLink>
-          <NavLink to='/cartpage' className='hover:text-orange-500 text-gray-500'><BsCart4 className='w-[50px] h-[35px] ' /></NavLink>
+          <NavLink to='/cartpage' className=' text-gray-500'>
+
+            <span className='absolute ml-[25px] top-[18px] z-50 text-white border w-[25px] flex justify-center bg-red-500 rounded-[50px]'>{totalCartItem}</span>
+            <BsCart4 className='w-[50px] h-[35px] ' /></NavLink>
+
+
         </div>
         <div className='flex  ml-[100px] space-x-[20px]'>
           <div className='flex space-x-[20px] mt-[8px]'>
--- src/Pages/CartPage.jsx
+++ src/Pages/CartPage.jsx
@@ -1,31 +1,57 @@
-import React, { useContext } from 'react'
-import { CartContext } from '../Context/CartContext/CartProvider'
+import { useContext } from "react";
+import { CartContext } from "../Context/CartContext/CartProvider";
+import { MdDelete } from "react-icons/md";
 
 function CartPage() {
-  const { state } = useContext(CartContext)
+  const { state } = useContext(CartContext);
   console.log(state);
   return (
     <div>
-      <h1>Cart page</h1>
       <div>
         {state.cartItem ? (
-          <div>
-            {state.cartItem.map((item) => {
-              return (
-                <div key={item.id}>
-                  <h1>{item.name}</h1>
-                  <img className="h-40" src={item.image} alt="" />
-                </div>
-              );
-            })}
+          <div className="flex"  >
+            <div className="w-[900px] "  >
+              {state.cartItem.map((item) => {
+                return (
+                  <div key={item.id}>
+                    <div className="   flex justify-evenly items-center m-5 h-56 rounded-2xl shadow-2xl shadow-black ">
+                      <div>
+                        <img className="h-36 rounded-2xl" src={item.image} alt="" />
+                      </div>
+                      <div className=" h-14 ">
+                        <h1>{item.name}</h1>
+
+                      </div>
+                      <div className=" h-14">
+                        <h1 className=" text-orange-600">  Rs. {item.caloriesPerServing}</h1>
+                        <button>
+                          <MdDelete color="red" size={30} />
+                        </button>
+                      </div>
+                      <div className=" h-14 space-x-2">
+                        <button className="bg-gray-100 rounded-[50px] px-2.5">-</button>
+                        <span>{item.qty}</span>
+                        <button className="bg-gray-100 rounded-[50px] px-2.5">+</button>
+
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <div className=" rounded-2xl w-80 mt-10 shadow-2xl shadow-gray-400 h-56 flex flex-col justify-center items-center gap-y-4" >
+              <h1 className="text-3xl">Order Summery</h1>
+              <h1>Subtotal(0 items)</h1>
+              <h1>Total</h1>
+              <button className="bg-orange-600 p-3 text-white rounded-2xl">Procced to checkout</button>
+            </div>
           </div>
         ) : (
-          <div>Item Not Found On Cart</div>
+          <div >Item Not Found On Cart</div>
         )}
       </div>
     </div>
-
-  )
+  );
 }
 
-export default CartPage
+export default CartPage;
